c712a9534651ab602edb6f256b2740c7b797a106
Pankaj Chandel <chandelpankaj2701@gmail.com>
2026-07-07 10:13:26 +0530

    Rotate Array By 90 Degree

---
+++ rotation_of_array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+
+    int arr[n][n];
+
+    for(int i= 0; i< n; i++){
+        for(int j= 0; j< n ;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i =0 ;i< n ;i++){
+        for(int j= 0; j< n ;j++){
+            cout<<arr[i][j]<<" ";
+        }cout<<endl;
+    }
+
+    for(int i= 0;i<n ;i++){
+        for(int j= i+1; j<n ; j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+
+    for(int i= 0; i< n; i++){
+        reverse(arr[i],arr[i]+n);
+    }
+
+    cout<<"rotated array "<<endl;
+    for(int i= 0; i< n ;i++){
+        for(int j= 0 ;j< n ;j++){
+            cout<<arr[i][j];
+        }cout<<endl;
+    }
+    return 0;
+}
